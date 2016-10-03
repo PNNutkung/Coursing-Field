89c1366d4de69d511c88d084a85df8cd2e099ee4
PNNutkung <pipatpol.t@ku.th>
2016-10-03 17:05:22 +0700

    Change database setting

--- coursing_field/settings.py
+++ coursing_field/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'coursing_field.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': './mysql.cnf'
+        },
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
