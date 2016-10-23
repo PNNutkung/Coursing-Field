80aab1aef7e563aa79d746d452f636d7a1e1e113
gafig <chawakorn.a@ku.th>
2016-10-23 14:21:17 +0700

    add register

--- login/urls.py
+++ login/urls.py
@@ -5,4 +5,6 @@ app_name = 'login'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^create_new_user/$', views.create_new_user, name='create_new_user'),
 ]
--- login/views.py
+++ login/views.py
@@ -6,13 +6,47 @@ def index(req):
     return render(req, 'login/login.html')
 
 def login(req):
-    """username = req.POST['username']
-    password = req.POST['password']"""
+    username = req.POST['username']
+    password = req.POST['password']
 
     """if username == "admin":
         return render(req, 'login.html')
     else:
-        return render(req, 'login/index.html', {
-            'error_message': 'Invalid username or password'
-        })"""
-    return render(req, 'login/login.html', {'error_message': 'Invalid username or password'})
+        return render(req, 'login/login.html', { 'error_message': 'Invalid username or password'})"""
+
+def register(req):
+    return render(req, 'login/register.html')
+
+def create_new_user(req):
+    firstname = req.POST['firstname']
+    lastname = req.POST['lastname']
+    username = req.POST['username']
+    password = req.POST['password']
+    confirm_password = req.POST['confirm_password']
+    bday = req.POST['bday']
+    gender = req.POST['gender']
+    user = None
+    return render(req, 'login/login.html', {
+        'error_message': 'Register successfully.'
+    })
+    """
+    try:
+        user = User.objects.get(username=username)
+    except(User.DoesNotExist):
+        if user is not None:
+            return render(req, 'login/register.html', {
+                'error_message': 'This username has already taken.'
+            })
+        elif password != confirm_password:
+            return render(req, 'login/register.html', {
+                'error_message': 'Your password is not match.'
+            })
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            return render(req, 'login/register.html', {
+                'error_message': 'Your email is not valid.'
+            })
+        else:
+            user = User.objects.create_user(username, email=email, password=password, first_name=firstname, last_name=lastname)
+            return render(req, 'login/index.html', {
+                'error_message': 'Register successfully.'
+            })"""
