7bb2d9e7aaffce730170bc33a1e7bc371a9be718
PNNutkung <pipatpol.t@ku.th>
2016-10-25 13:15:45 +0700

    Add encrypt s3

--- coursing_field/settings.py
+++ coursing_field/settings.py
@@ -105,13 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
