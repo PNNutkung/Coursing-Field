45daef2038b1a1c6fd8ce16d3d32c900d05dfd55
PNNutkung <pipatpol.t@ku.th>
2016-10-25 14:28:27 +0700

    Add upload thumnail form to create course

--- course/views.py
+++ course/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from mainmodels.models import Category
+from mainmodels.models import Category, Course
 # Create your views here.
 def createCourse(req):
     course_category = Category.objects.all()
     return render(req, 'course/createCourse.html', {'course_category':course_category})
 
 def createNewCourse(req):
+    #new_course = 
     return redirect(reverse('course:createCourse'))
