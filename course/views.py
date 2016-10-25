cbc483463b10984763b1b80e24665410350b40df
PNNutkung <pipatpol.t@ku.th>
2016-10-25 14:07:13 +0700

    Can pull category from database

--- course/views.py
+++ course/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from mainmodels.models import Category
 # Create your views here.
 def createCourse(req):
-    course_category = [{'value':1,'name':'Computer'},{'value':2,'name':'Art'}]
+    course_category = Category.objects.all()
     return render(req, 'course/createCourse.html', {'course_category':course_category})
 
 def createNewCourse(req):
