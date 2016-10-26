af13b09cf14bd22b171236f6f0250e062054f398
Pakpon Jetapai <pakpon.j@ku.th>
2016-10-26 22:52:19 +0700

    owner of a course can edit his/her course information (except category)

--- course/urls.py
+++ course/urls.py
@@ -5,4 +5,6 @@ app_name = 'course'
 urlpatterns = [
     url(r'^create/$', views.createCourse, name='createCourse'),
     url(r'^(?P<courseID>[0-9]+)/$', views.view_course, name='view_course'),
+    url(r'^(?P<courseID>[0-9]+)/manage/$', views.manage_course, name='manage_course'),
+    url(r'^(?P<courseID>[0-9]+)/edited_course/$', views.edited_course, name='edited_course'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from mainmodels.models import Category, Course, CourseInCategory, TakenCourse
+from mainmodels.models import Category, Course, CourseInCategory, TakenCourse, Video, CoursePreview
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -13,7 +13,7 @@ def createCourse(req):
             courseDesc = req.POST['courseDesc']
             courseThumbnail = req.FILES['courseThumbnail']
             coursePrice = req.POST['coursePrice']
-            owner = User.objects.get(username='nut')
+            owner = req.user
 
             newCourse = Course(courseName=courseName, courseDesc=courseDesc,courseThumbnail=courseThumbnail, owner=owner, coursePrice=coursePrice, isDelete=False)
             newCourse.save()
@@ -32,6 +32,9 @@ def createCourse(req):
 def view_course(req, courseID):
     if req.user.is_authenticated:
         course = Course.objects.get(courseID=courseID)
+        isOwner = False
+        if req.user.id == course.owner.id:
+            isOwner = True
         hasTakencourse = False
         try:
             takenCourse = TakenCourse.objects.get(course=course,taker=req.user)
@@ -41,6 +44,51 @@ def view_course(req, courseID):
             hasTakencourse = False
         userWithProfile = User.objects.get(id=req.user.id)
         inCategory = CourseInCategory.objects.get(course=course).category.categoryName
-        return render(req, 'course/viewCourse.html', {'course' : course, 'hasTakenCourse' : hasTakencourse, 'inCategory' : inCategory, 'userWithProfile' : userWithProfile})
+        return render(req, 'course/viewCourse.html', {'course' : course, 'hasTakenCourse' : hasTakencourse, 'isOwner' : isOwner, 'inCategory' : inCategory, 'userWithProfile' : userWithProfile})
     else:
         return redirect(reverse('mockaccount:index'))
+
+def manage_course(req, courseID):
+    if req.user.is_authenticated:
+        course = Course.objects.get(courseID=courseID)
+        videos = Video.objects.filter(course=course)
+        inCategory = CourseInCategory.objects.get(course=course).category.categoryName
+        isOwner = False
+        if req.user.id == course.owner.id:
+            isOwner = True
+        else:
+            return redirect(reverse('course:view_course'))
+        return render(req, 'watchvideo/show_content_in_tabs.html',{ 'course' : course, 'videos' : videos, 'isOwner' : isOwner, 'inCategory' : inCategory })
+    else:
+        return redirect(reverse('mockaccount:index'))
+
+def edited_course(req, courseID):
+    if req.method == 'POST':
+        course = Course.objects.get(courseID=courseID)
+        newCourseName = req.POST.get('courseName')
+        if newCourseName is not None:
+            course.courseName = newCourseName
+        # Missing Category might have to delete old one before insert new CourseInCategory
+        newPreviewVideo = req.FILES.get('previewVideo')
+        if newPreviewVideo is not None:
+            try:
+                #Assume there is only one preview video. Any old ones should be labeled isDelete=True
+                coursePreview = CoursePreview.objects.get(course=course,isDelete=False)
+                #Add for loop to set old preview videos as isDelete=True
+                coursePreview.isDelete = True
+                coursePreview.save()
+            except ObjectDoesNotExist:
+                print("Object does not exist creating new one")
+            finally:
+                newCoursePreview = CoursePreview(course=course,previewVideo=newPreviewVideo,isDelete=False)
+                newCoursePreview.save()
+        newCourseThumbnail = req.FILES.get('courseThumbnail')
+        if newCourseThumbnail is not None:
+            course.courseThumbnail = newCourseThumbnail
+        newCourseDesc = req.POST.get('courseDesc')
+        if newCourseDesc is not None:
+            course.courseDesc = newCourseDesc
+        course.save()
+        return redirect(reverse('course:manage_course'))
+    else:
+        return HttpResponse('Failed to post.')
