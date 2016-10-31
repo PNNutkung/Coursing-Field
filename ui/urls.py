5e02293056b978a1171c8a719d0f1096e514592a
Thanawit <thanawit.g@ku.th>
2016-10-31 20:53:42 +0700

    combine ui app

---
+++ ui/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'ui'
+
+urlpatterns = [
+
+    url(r'^$' , views.index , name='index'),
+    url(r'^view$' , views.view , name='view'),
+    url(r'^browse$' , views.browse , name='browse'),
+    url(r'^register$' , views.register , name='register'),
+    url(r'^create$' , views.create , name='create'),
+
+]
