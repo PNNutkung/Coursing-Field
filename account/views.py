a67b903f9d75cd26969bd76c90847a323ae46b20
PNNutkung <pipatpol.t@ku.th>
2016-11-03 01:42:43 +0700

    Add regex checking on register page

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ import django.contrib.auth as auth
 from mainmodels.models import Profile, Course, Category
 from django.urls import reverse
 from datetime import datetime
+import re
 
 # Create your views here.
 def login(req):
@@ -20,10 +21,33 @@ def login(req):
         return render(req, 'account/login.html', {'pageTitle': 'Login'})
 
 def register(req):
-    return render(req, 'account/register.html')
+    if req.method == 'POST':
+        try:
+            username = req.POST['username']
+            password = req.POST['password']
+            firstname = req.POST.get('firstname')
+            lastname = req.POST.get('lastname')
+            email = req.POST.get('email')
+            gender = req.POST.get('gender', 'M')
+            profilePicture = req.FILES.get('profilepicture')
+            birthDate_str = req.POST.get('birthday')
+            birthDate = datetime.strptime(birthDate_str, '%Y-%m-%d').date()
+            user = User.objects.create_user(username=username,password=password,email=email,first_name=firstname,last_name=lastname)
+            user.profile.gender = gender[0]
+            user.profile.profilePicture = profilePicture
+            user.profile.birthDate = birthDate
+            user.save()
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(req, user)
+                return redirect(reverse('account:profile'))
+        except IntegrityError:
+            return render(req, 'account/register.html', {'message': 'This username has  already taken.'})
+    else:
+        return render(req, 'account/register.html')
 
 def profile(req):
-    teachingCourse = Course.objects.filter(owner=req.user)
+    #teachingCourse = Course.objects.filter(owner=req.user)
     return render(req, 'account/profile.html')
 
 def logout(req):
