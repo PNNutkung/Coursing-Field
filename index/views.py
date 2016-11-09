14a17e4f14408c9e7ff0e839adb687ea8c385291
PNNutkung <pipatpol.t@ku.th>
2016-11-09 17:37:58 +0700

    Add popular query

--- index/views.py
+++ index/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from mainmodels.models import Course, Review
 # Create your views here.
 def index(req):
-    return render(req, 'ui/index.html', {'pageTitle': 'Coursing Field'})
+    mostPopularCourses = Course.objects.raw('SELECT * FROM mainmodels_course as main_course JOIN (SELECT main_review.course_id, AVG(main_review.rating) as "average_rating" FROM mainmodels_review as main_review) as avg_main_review ON main_course.courseID = avg_main_review.course_id ORDER BY avg_main_review.average_rating;')
+    return render(req, 'index/main.html', {'pageTitle': 'Coursing Field', 'mostPopularCourses': mostPopularCourses})
