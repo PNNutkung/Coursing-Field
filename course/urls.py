57bd01ebe2998f7a649a5d0690020223efa26cce
Pakpon Jetapai <pakpon.j@ku.th>
2016-11-09 17:18:20 +0700

    fixed video with only audio found out that it has unsupported video encoding.

--- course/urls.py
+++ course/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^(?P<courseID>[0-9]+)/manage/$', views.manage_course, name='manage_course'),
     url(r'^(?P<courseID>[0-9]+)/edited_course/$', views.edited_course, name='edited_course'),
     url(r'^(?P<courseID>[0-9]+)/upload/$', views.upload_video, name='upload_video'),
+    url(r'^(?P<courseID>[0-9]+)/(?P<videoID>[0-9]+)/edited_video/$', views.edited_video, name='edited_video'),
     url(r'^(?P<courseID>[0-9]+)/(?P<videoID>[0-9]+)/comment/$', views.comment_on_video, name='comment_on_video'),
     url(r'^(?P<courseID>[0-9]+)/reorderedvideo/$', views.reordered_video, name='reordered_video'),
     url(r'^review/(?P<courseID>[0-9]+)/$', views.reviewCourse, name='reviewCourse'),
