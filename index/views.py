5bf6d25148f627cd0e56b3530fe4c6c2fca6d913
PNNutkung <pipatpol.t@ku.th>
2016-11-09 17:47:16 +0700

    Add LIMIT to 10 rows

--- index/views.py
+++ index/views.py
@@ -3,5 +3,5 @@ from django.urls import reverse
 from mainmodels.models import Course, Review
 # Create your views here.
 def index(req):
-    mostPopularCourses = Course.objects.raw('SELECT * FROM mainmodels_course as main_course JOIN (SELECT main_review.course_id, AVG(main_review.rating) as "average_rating" FROM mainmodels_review as main_review) as avg_main_review ON main_course.courseID = avg_main_review.course_id ORDER BY avg_main_review.average_rating;')
+    mostPopularCourses = Course.objects.raw('SELECT * FROM mainmodels_course as main_course JOIN (SELECT main_review.course_id, AVG(main_review.rating) as "average_rating" FROM mainmodels_review as main_review) as avg_main_review ON main_course.courseID = avg_main_review.course_id ORDER BY avg_main_review.average_rating LIMIT 10;')
     return render(req, 'index/main.html', {'pageTitle': 'Coursing Field', 'mostPopularCourses': mostPopularCourses})
