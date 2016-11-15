3ce75de405afe073fb5d029c1e557426dd1548dd
PNNutkung <pipatpol.t@ku.th>
2016-11-15 13:26:39 +0700

    start search app

--- mainmodels/models.py
+++ mainmodels/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from decimal import Decimal
-from haystack import indexes
 
 def get_upload_path_thumbnail(instance, filename):
     name, ext = filename.split('.')
