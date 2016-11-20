bfe6c001d1af5dc3abdc04b94f7fe581d3575d84
carymono <thanawit.g@ku.th>
2016-11-20 20:51:07 +0700

    can now feature but no guard condition

--- watchvideo/urls.py
+++ watchvideo/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^(?P<courseID>[0-9]+)/content/$', views.show_content_in_tabs, name='show_contents_in_tabs'),
     url(r'^(?P<courseID>[0-9]+)/content/overview/$', views.overview, name='overview'),
     url(r'^(?P<courseID>[0-9]+)/content/(?P<videoID>[0-9]+)/$', views.video_tab, name='video_tab'),
+    url(r'^(?P<courseID>[0-9]+)/upgrade/$', views.upgrade_course, name='upgrade_course'),
 ]
